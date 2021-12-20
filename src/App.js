d9c71e995e4b88cd64a769db1fe5c6a95ed97e75
hooman <hoomanm87@gmail.com>
2021-12-20 19:23:18 +0330

    feat: Import Header and Main components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Component } from "react";
+import Header from "./components/HeaderContainer";
+import MainContainer from "./components/MainContainer";
 
 class App extends Component {
     render() {
-        return(
-            <></>
+        return (
+            <>
+                <Header />
+                <MainContainer />
+            </>
         )
     }
 }

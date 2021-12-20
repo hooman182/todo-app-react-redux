b33469d0f6e1efd278a6c8f8c8c82719d550d110
hooman <hoomanm87@gmail.com>
2021-12-20 19:22:52 +0330

    feat: Create Header and Main components

---
+++ src/components/HeaderContainer.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <header class="section">
+                <p class="title">
+                    Basic Todo App
+                </p>
+                <p class="subtitle">
+                    Using redux
+                </p>
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <main className="container">
+            </main>
+        )
+    }
+}
+
+export default MainContainer;

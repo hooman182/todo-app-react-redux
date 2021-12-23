9f8d41ca3d5d8a13b9babfdbe55a4b058ba527c4
hooman <hoomanm87@gmail.com>
2021-12-23 14:29:11 +0330

    feat: Create events and pass data to store

--- src/components/todo/Form.jsx
+++ src/components/todo/Form.jsx
@@ -1,15 +1,34 @@
 import { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo } from "../../actions";
 
 class Form extends Component {
+    state = {
+        input: ""
+    }
+    formSubmitHandle = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.input);
+        this.setState({ input: "" })
+    }
+    inputChangeHandle = (e) => {
+        this.setState({ input: e.target.value })
+    }
     render() {
         return (
-            <form method="post">
+            <form method="post" onSubmit={this.formSubmitHandle}>
                 <div className="control">
-                    <input className="input is-normal" type="text" placeholder="What needs to be done?" />
+                    <input
+                        className="input is-normal"
+                        type="text"
+                        placeholder="What needs to be done?"
+                        value={this.state.input}
+                        onChange={this.inputChangeHandle}
+                    />
                 </div>
             </form>
         )
     }
 }
 
-export default Form;
+export default connect(null, { addTodo })(Form);

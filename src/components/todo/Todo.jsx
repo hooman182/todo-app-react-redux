476435b1fda3325802db94ee6ab6a16705eed456
hooman <hoomanm87@gmail.com>
2021-12-23 14:29:55 +0330

    feat: Create events and show data in Todo component

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,22 +1,56 @@
 import { Component } from "react";
+import { connect } from "react-redux";
+import { deleteTodo, editTodo } from "../../actions";
 
 class Todo extends Component {
+    state = {
+        edit: false,
+        input: "",
+        id: null
+    }
+    deleteTodoClick = (index) => {
+        this.props.deleteTodo(index);
+    }
+    editTodoClick = (index) => {
+        this.setState({ edit: !this.state.edit, id: index })
+    }
+    formSubmitHandle = (e) => {
+        e.preventDefault();
+        this.props.editTodo(this.state.input, this.props.data.id);
+        this.setState({ edit: false })
+    }
+    inputChangeHandle = (e) => {
+        this.setState({ input: e.target.value });
+    }
     render() {
+        const todo = this.props.data;
         return (
             <div className="columns">
                 <div className="column is-10">
-                    <label className="checkbox">
-                        <input type="checkbox" className="mr-2" />
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    </label>
+                    {
+                        !this.state.edit ?
+                            <label className="checkbox">
+                                <input type="checkbox" className="mr-2" />
+                                {todo.text}
+                            </label>
+                            :
+                            <form method="post" onSubmit={this.formSubmitHandle}>
+                                <input className="input is-small is-warning" type="text" value={this.state.input} onChange={this.inputChangeHandle} />
+                            </form>
+                    }
                 </div>
                 <div className="column">
-                    <div class="buttons has-addons are-small">
-                        <button class="button is-danger">
-                            <i class="fas fa-trash"></i>
+                    <div className="buttons has-addons are-small">
+                        <button className="button is-danger" onClick={this.deleteTodoClick.bind(this, todo.id)}>
+                            <i className="fas fa-trash"></i>
                         </button>
-                        <button class="button is-info">
-                            <i class="fas fa-pen"></i>
+                        <button className={`button ${!this.state.edit ? 'is-info' : 'is-warning' }`} onClick={this.editTodoClick.bind(this, todo.id)}>
+                            {
+                                !this.state.edit ?
+                                    <i className="fas fa-pen"></i>
+                                    :
+                                    <i class="fas fa-times"></i>
+                            }
                         </button>
                     </div>
                 </div>
@@ -25,4 +59,4 @@ class Todo extends Component {
     }
 }
 
-export default Todo;
+export default connect(null, { deleteTodo, editTodo })(Todo);
